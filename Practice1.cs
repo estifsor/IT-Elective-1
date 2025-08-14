36696c08c77802aeda305f56c65d37641fb6a816
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-14 15:29:37 +0800

    Add files via upload

---
+++ Practice1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICE
+{
+    internal class Practice1
+    {
+        static void Main(string[] args)
+        {
+            //Practice 1: Write an if statement to check if a number is even. If it is even, display the message "The number [number] is even."
+
+            Console.WriteLine("Input a number to check if it is even:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number " + number + " is even.");
+
+                    }
+        }
+    }
+}
