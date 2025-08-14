54f70a53f3d9a77d089d255f1ac29e5212c52581
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-14 15:30:55 +0800

    Add files via upload

---
+++ Practice2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Practice2
+    {
+        static void Main(string[] args)
+        {
+            //Practice  2: Write an if-else statement to determine if a number is positive or negative. If the number is positive. display the message 
+            // "the number [number] is positive." Otherwise, display the message "The number [number] is negative."
+            
+            Console.WriteLine("Input a number to check if it is positive or negative:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                Console.WriteLine("The number " + number + " is positive.");
+            }
+            else
+            {
+                Console.WriteLine("The number " + number + " is negative");
+            }
+
+        }
+    }
+}
